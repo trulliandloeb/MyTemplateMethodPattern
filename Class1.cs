9442d3176f3593bc786eb8c04dbd69f4429959f1
trulliandloeb <trulliandloeb@hotmail.com>
2011-02-13 14:40:12 +0800

    fisrst commit

---
+++ Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace MyTemplateMethodPattern
+{
+    public interface ITemplateMethod
+    { }
+
+    public static class TemplateMethodExtension
+    {
+        public static void TemplateMethod(this ITemplateMethod iTemplate, Action actionOne, Action actionTwo)
+        {
+            actionOne();
+            actionTwo();
+        }
+    }
+
+    namespace Test
+    {
+        public class TemplateMethod : ITemplateMethod
+        {
+
+        }
+
+        [TestFixture]
+        public class Test
+        {
+            [Test]
+            public void TestExample()
+            {
+                var t = new TemplateMethod();
+                t.TemplateMethod(() => Console.WriteLine("hello"), () => Console.WriteLine("world"));
+            }
+        }
+    }
+}
